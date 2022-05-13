b28743d2a26b75930201b7d0d31614872b952e3f
15114818657@163.com <605715694>
2022-05-13 10:23:53 +0800

    二分查找-查找出现的第一个元素

--- src/main/java/top/lconcise/arithmetic/Search.java
+++ src/main/java/top/lconcise/arithmetic/Search.java
@@ -1,7 +1,7 @@
 package top.lconcise.arithmetic;
 
 /**
- * 二分查找
+ * 二分查找。
  *
  * @author: liusj
  * @date: 2022/5/11
@@ -9,11 +9,39 @@ package top.lconcise.arithmetic;
 public class Search {
 
     public static void main(String[] args) {
-        int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 9};
+        int[] array = {0, 1, 2, 2, 2, 2, 3, 4, 5, 6, 7, 9};
 
-        System.out.println(bSearch(array, 2));
+        System.out.println(bSearch02(array, 2));
     }
 
+    // 二分查找，有序且存在重复元素，查找第一个出现的元素
+    public static int bSearch02(int[] array, int val) {
+        return bSearchInternally02(array, 0, array.length - 1, val);
+    }
+
+    private static int bSearchInternally02(int[] array, int low, int high, int val) {
+        if (low > high) return -1;
+
+        int mid = (low + high) / 2;
+
+        if (val > array[mid]) {
+            low = mid + 1;
+            return bSearchInternally02(array, low, high, val);
+        } else if (val < array[mid]) {
+            high = mid - 1;
+            return bSearchInternally02(array, low, high, val);
+        } else if (val == array[mid]) {
+            if (0 == mid || array[mid - 1] != val) {
+                return mid;
+            } else {
+                high = mid - 1;
+                return bSearchInternally02(array, low, high, val);
+            }
+        }
+        return -1;
+    }
+
+    // 二分查找，适用于有序且不重复的数组
     public static int bSearch(int[] a, int val) {
         return bSearchInternally(a, 0, a.length - 1, val);
     }
